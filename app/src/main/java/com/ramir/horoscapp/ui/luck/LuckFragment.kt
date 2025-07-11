996ba81449f74e245d9cfa9275e782c932e04840
Ramiro404 <ramirocruznavarro@hotmail.com>
2025-07-11 15:08:15 -0600

    Initial commit

---
+++ app/src/main/java/com/ramir/horoscapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,25 @@
+package com.ramir.horoscapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ramir.horoscapp.databinding.FragmentLuckBinding
+
+
+class LuckFragment : Fragment() {
+    private lateinit var  _binding: FragmentLuckBinding
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/ramir/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,26 @@
+package com.ramir.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ramir.horoscapp.R
+import com.ramir.horoscapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+    private lateinit var _binding: FragmentPalmistryBinding
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentPalmistryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
